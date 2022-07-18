bd733ef60c48334949624fe0b863e7e67d264541
chosunghyun18 <krismon@naver.com>
2022-07-19 00:45:18 +0900

    [Edit]pull and edit placerepo

--- src/main/java/hackathon/nomadworker/repository/PlaceRepository.java
+++ src/main/java/hackathon/nomadworker/repository/PlaceRepository.java
@@ -13,24 +13,21 @@ import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
-public class PlaceRepository
-{
+public class PlaceRepository {
     private final EntityManager em;
 
-    public Place findOne(Long id)
-    {
-        return  em.find(Place.class,id);
+    public Place findOne(Long id) {
+        return em.find(Place.class, id);
     }
 
-    public List<Place> findAll()
-    {
-        return em.createQuery("select p from Place p",Place.class)
+    public List<Place> findAll() {
+        return em.createQuery("select p from Place p", Place.class)
                 .getResultList();
     }
 
     public List<Place> findOneByNickName(String palceName) {
         String jpql = "select p from Place p where p.p_name like :palceName";
-        TypedQuery<Place> query = em.createQuery(jpql, Place.class).setParameter("palceName",palceName ).setMaxResults(1000);
+        TypedQuery<Place> query = em.createQuery(jpql, Place.class).setParameter("palceName", palceName).setMaxResults(1000);
         return query.getResultList();
     }
 
