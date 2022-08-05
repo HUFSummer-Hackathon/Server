1b6dc2f1101dd54b18220fa226b99aef8218fcf7
DonghyunShin <shindh0429@gmail.com>
2022-08-06 00:43:24 +0900

    Image 수정

--- src/main/java/hackathon/nomadworker/api/ImageUploadController.java
+++ src/main/java/hackathon/nomadworker/api/ImageUploadController.java
@@ -6,6 +6,7 @@ import hackathon.nomadworker.dto.ImageDtos.*;
 import hackathon.nomadworker.service.FileUploadService;
 import hackathon.nomadworker.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,8 +17,8 @@ public class ImageUploadController {
     private final FileUploadService fileUploadService;
     private final UserService userService;
 
-    @PutMapping("/api/user/profile")
-    public PutResponse uploadImage(@RequestHeader("Authorization") String u_uid, @RequestPart("file") MultipartFile file)
+    @PutMapping(value = "/api/user/profile", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public PutResponse uploadImage(@RequestHeader("Authorization") String u_uid, @RequestPart(value = "file", required = false) MultipartFile file)
     {
         String imageUrl =  fileUploadService.uploadImage(file);
         User result = userService.userImagePost(u_uid, imageUrl);
